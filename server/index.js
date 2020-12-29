ee357effa3aa789f7a66a619517e40f79cefaf64
jacoborbach <jacoborbach96@gmail.com>
2020-12-29 18:37:47 -0500

    finished

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const products = require('../products.json')
 const getProducts = require('./getProducts');
+const getProduct = require('./getProduct');
 
 const app = express()
 
 const port = 3002
 
 app.get('/api/products', getProducts)
+app.get('/api/products/:id', getProducts)
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`)
